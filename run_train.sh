333e42af516fe729bdc49351467cbf5b1a884718
Chao Huang <mister.huangchao@gmail.com>
2026-01-22 06:55:25 -0800

    run multiple RD points

--- run_train.sh
+++ run_train.sh
@@ -1,8 +1,8 @@
+export CUDA_VISIBLE_DEVICES=3
 python train.py \
---model_name HPCM_Large \
+--model_name HPCM_Base \
 --train_dataset /data/chaohuang/hpcm/data/flicker2W \
---test_dataset /data/chaohuang/hpcm/data/DIV2K256 \
---num-workers 4 \
---lambda 0.013 \
+--test_dataset /data/chaohuang/hpcm/data/DIV2K_valid_256 \
 --batch-size 8 \
---epochs 750
+--epochs 750 \
+--lambda 0.0483
